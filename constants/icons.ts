5928cd2dec2767e0e895c2a178e1fa4601ed6344
Irene <mijeong.irene.ban@gmail.com>
2024-10-03 19:22:35 -0700

    tab bar fix

---
+++ constants/icons.ts
@@ -0,0 +1,7 @@
+import newIcon from '../assets/icons/new.png';
+import dashboardIcon from '../assets/icons/dashboard.png';
+import collectionIcon from '../assets/icons/collection.png';
+import exploreIcon from '../assets/icons/explore.png';
+import profileIcon from '../assets/icons/profile.png';
+
+export default { newIcon, dashboardIcon, collectionIcon, exploreIcon, profileIcon };
