884cf3d0cbb50257ade9b1c524527b65a3099f56
jun-5 <rkd7997@gmail.com>
2024-10-06 23:05:43 +0900

    add calendar Icon

---
+++ constants/icons.ts
@@ -0,0 +1,8 @@
+import newIcon from '../assets/icons/new.png';
+import dashboardIcon from '../assets/icons/dashboard.png';
+import collectionIcon from '../assets/icons/collection.png';
+import exploreIcon from '../assets/icons/explore.png';
+import profileIcon from '../assets/icons/profile.png';
+import calendarIcon from '../assets/icons/calendar.png';
+
+export default { newIcon, dashboardIcon, collectionIcon, exploreIcon, profileIcon, calendarIcon };
